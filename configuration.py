b8b708a3885b51c6281e5c27cc75c24889ae52aa
Brian Vohaska <bvohaska@users.noreply.github.com>
2023-03-13 12:43:13 -0400

    updated config with text triggers

--- configuration.py
+++ configuration.py
@@ -8,7 +8,7 @@ import logging
 ###########
 
 # Set the logging level
-DISCORD_BOT_LOG_LEVEL = logging.DEBUG
+DISCORD_BOT_LOG_LEVEL = logging.INFO
 
 # Configure the logging level
 logging.basicConfig(level=DISCORD_BOT_LOG_LEVEL)
@@ -16,10 +16,27 @@ logging.basicConfig(level=DISCORD_BOT_LOG_LEVEL)
 # Create a default logger with this configuration
 DISCORD_BOT_LOGGER = logging.getLogger(__name__)
 
+############################
+# Discord Message Patterns #
+############################
+
+# Text Trigger Patterns
+SIGN_MESSAGE = '$sign '
+VERIFY_MESSAGE = '$verify '
+QR_SIGN = '$qrsign '
+QR_VERIFY = '$qrverify '
+END_MSG = '!ENDMSG'
+# Length of trigger patters for truncation
+SIGN_TRUNCATE = len(SIGN_MESSAGE)
+VERIFY_TRUNCATE = len(VERIFY_MESSAGE)
+QR_SIGN_TRUNCATE = len(QR_SIGN)
+QR_VERIFY_TRUNCATE = len(QR_VERIFY)
 
 ########################
 # Constants & Literals #
 ########################
+# Set the size of the QR code
+QR_CODE_SIZE = 3
 
 # Set the standard text encoding; this is used for converting back and forth from strings to bytes
 STANDARD_ENCODING = 'utf-8'
